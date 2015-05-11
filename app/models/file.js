32fd5bd1b39863243eff92ac75a3b14ec70c74a4
Victor Lin <bornstub@gmail.com>
2015-05-10 22:35:58 -0700

    Display source code

--- app/models/file.js
+++ app/models/file.js
@@ -4,5 +4,9 @@ export default DS.Model.extend({
   breaks: DS.hasMany('break'),
   session: DS.belongsTo('session'),
   filename: DS.attr('string'),
-  content: DS.attr('string')
+  content: DS.attr('string'),
+  
+  source_code: function() {
+    return atob(this.get('content'));
+  }.property('content')
 });
