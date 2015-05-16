dc3de44649f3d652d7f3c0ce953c5b3983ae3cca
Victor Lin <bornstub@gmail.com>
2015-05-16 15:36:03 -0700

    Wrap code lines with span

--- app/components/highlight-js.js
+++ app/components/highlight-js.js
@@ -8,6 +8,72 @@ function range(start, count) {
     });
 }
 
+// return tag opening string
+function tagOpen(tag) {
+  return '<%@ class="%@">'.fmt(tag.tagName.toLowerCase(), tag.className)
+}
+
+// return tag closing string
+function tagClose(tag) {
+  return '</%@>'.fmt(tag.tagName.toLowerCase());
+}
+
+// split DOM nodes into lines
+function splitLines(nodes) {
+  var lines = [];
+  var currentLine = [];
+  var openingTags = [];
+  var traverse = function (currentNodes) {
+    var lastOpenTag;
+    var tagPrefix = [];
+    var tagSuffix = [];
+    openingTags.forEach(function (tag) {
+      var tagName = tag.tagName.toLowerCase();
+      tagPrefix.push(tagOpen(tag));
+      tagSuffix.push(tagClose(tag));
+    });
+    tagPrefix = tagPrefix.join('');
+    tagSuffix = tagSuffix.join('');
+    if (openingTags.length) {
+      lastOpenTag = openingTags[openingTags.length - 1];
+      currentLine.push(tagOpen(lastOpenTag));
+    }
+    for (var i = 0; i < currentNodes.length; i++) {
+      var node = currentNodes[i];
+      // text node
+      if (node.nodeType === Node.TEXT_NODE) {
+        var lineParts = node.textContent.match(/([^\n]*?)(\n|$)/g);
+        lineParts.forEach(function (line) {
+          // we see a newline, flush current line
+          if (line.endsWith('\n')) {
+            currentLine.push(line.slice(0, line.length - 1));
+            // wrap current line with open tags
+            lines.push(currentLine.join('') + tagSuffix);
+            currentLine = [tagPrefix];
+          // this line has no newline, just buffer it
+          } else {
+            currentLine.push(line);
+          }
+        });
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        openingTags.push(node);
+        traverse(node.childNodes, openingTags);
+      } else {
+        throw new Error('Not supported node %@'.fmt(node));
+      }
+    }
+    if (openingTags.length) {
+      currentLine.push(tagClose(lastOpenTag));
+      openingTags.pop();
+    }
+  };
+  traverse(nodes);
+  if (currentLine.length) {
+    lines.push(currentLine.join(''));
+  }
+  return lines;
+}
+
 export default Ember.Component.extend({
   classNameBindings: ['lang'],
   hasLineNumbers: true,
@@ -28,6 +94,13 @@ export default Ember.Component.extend({
     if (!lang) {
       throw new Error('highlight-js lang property must be set');
     }
-    return highlight(lang, code).value;
+    var html = highlight(lang, code).value;
+    var nodes = Ember.$.parseHTML(html);
+    var lines = splitLines(nodes);
+    var wrappedLines = [];
+    lines.forEach(function (line, index) {
+      wrappedLines.push('<span id="line-%@">'.fmt(index + 1) + line + '</span>');
+    });
+    return wrappedLines.join('\n');
   }).readOnly()
 });
