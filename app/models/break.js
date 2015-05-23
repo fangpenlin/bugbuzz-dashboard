3220926c1e24c3062cf2cc78f476ec212d9cc704
Victor Lin <bornstub@gmail.com>
2015-05-22 22:48:32 -0700

    Display local vars

--- app/models/break.js
+++ app/models/break.js
@@ -1,7 +1,19 @@
+import ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   session: DS.belongsTo('session'),
   file: DS.belongsTo('file', { async: true }),
-  lineno: DS.attr('number')
+  lineno: DS.attr('number'),
+  local_vars: DS.attr(),
+
+  // local vars as an array
+  localVarsArray: Ember.computed('local_vars', function () {
+    var arr = [];
+    var localVars = this.get('local_vars');
+    for(var key in localVars) {
+        arr.push({key: key, value: localVars[key]});
+    }
+    return arr;
+  })
 });
