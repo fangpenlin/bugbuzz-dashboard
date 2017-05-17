89f218503e70de3371a405684b5982e09d680f7d
Fang-Pen Lin <bornstub@gmail.com>
2017-05-16 22:02:35 -0700

    Upgrade ember

---
+++ ember-cli-build.js
@@ -0,0 +1,46 @@
+/* eslint-env node */
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+  app.import('bower_components/jquery-cookie/jquery.cookie.js');
+  app.import('bower_components/pubnub/web/pubnub.js');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+    destDir: 'assets'
+  });
+  app.import('bower_components/ember-shortcuts/ember-shortcuts.js');
+
+  app.import('vendor/highlight.js/styles/' + 'monokai_sublime' + '.css');
+  app.import('vendor/highlight.js/index.js', {
+    exports: {
+      'highlight.js': [
+        'default',
+        'highlight',
+        'highlightAuto',
+        'highlightBlock'
+      ]
+    }
+  });
+
+  app.import('vendor/aes-arraybuffer/aes.js');
+  app.import('vendor/aes-arraybuffer/crypto.js');
+
+  return app.toTree();
+};
