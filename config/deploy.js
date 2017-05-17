274af23f4d509f240bb27e9544debdb96ed16b57
Fang-Pen Lin <bornstub@gmail.com>
2017-05-16 23:34:48 -0700

    Add ember deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,11 +1,43 @@
-module.exports = {
-  production: {
-    store: {},
-    assets: {
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+    ENV.s3 = {
       accessKeyId: process.env['AWS_ACCESS_KEY'],
       secretAccessKey: process.env['AWS_SECRET_KEY'],
       bucket: 'dashboard.bugbuzz.io',
       region: 'us-west-1'
     }
+    ENV['s3-index'] = {
+      accessKeyId: process.env['AWS_ACCESS_KEY'],
+      secretAccessKey: process.env['AWS_SECRET_KEY'],
+      bucket: 'dashboard.bugbuzz.io',
+      region: 'us-west-1'
+    }
+    ENV.pipeline = {
+      activateOnDeploy: true
+    }
   }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
 };
